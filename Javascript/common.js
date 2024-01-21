8289d1ad7e2aad9e6ac7af91b01eebf49d2aa253
Taaheer Labbe <99655331+taaheer@users.noreply.github.com>
2024-01-21 19:15:23 +0530

    add common.js file

---
+++ Javascript/common.js
@@ -0,0 +1,16 @@
+    // Function to load HTML content
+    function loadHTML(url, targetSelector) {
+        fetch(url)
+          .then(response => response.text())
+          .then(html => {
+            const targetElement = document.querySelector(targetSelector);
+            targetElement.innerHTML += html; // Append instead of replacing
+          })
+          .catch(error => console.error('Error loading HTML:', error));
+      }
+  
+      // Load header and footer
+      window.onload = function() {
+        loadHTML('header.html', 'header');
+        loadHTML('footer.html', 'footer');
+      };
